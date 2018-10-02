1a8ed1384e1377a6cbb6decf244eaa8c77b2481b
Kokomben <etemol-3@student.ltu.se>
2018-10-02 08:48:15 +0200

    gjorde bootstrapping i mainen

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,9 @@ RUN apt-get -y upgrade
 
 RUN apt-get install -y \
 net-tools inetutils-traceroute apt-utils \
-iputils-ping xinetd telnetd
+iputils-ping xinetd telnetd golang-go
 
 RUN mkdir /home/go/src/kadlab
+RUN export GOROOT=/home/go/
 ADD . /home/go/src/kadlab
-WORKDIR /home/go/src/kadlab
+WORKDIR /home/go/src/kadlab/
--- main.go
+++ main.go
@@ -6,10 +6,45 @@ import (
 	"fmt"
 	kad "kadlab/d7024e"
 	"time"
+	"log"
+	"net"
 )
 
 func main() {
+	//dockermain()
+	mymain()
 
+}
+
+func dockermain(){
+	//Fixa IP
+	myIP := GetOutboundIP()
+	fmt.Println(myIP.String() + ":8080")
+
+	//Skapa main kontakt
+	mainID := kad.NewKademliaID("FFFFFFFFFFF11111111111111111111111111005")
+	maincontact := kad.NewContact(mainID,"172.19.0.2:8080")
+
+	if myIP.String() == "172.19.0.2" { //Jag är main
+		node := kad.NewKademlia(maincontact)
+		go node.GetNetwork().Listen(maincontact) //Starta main listen
+		node.GetNetwork().GetRT().PrintRoutingTable() //Printa min RT
+	}else{
+		time.Sleep(10 * 1000 * time.Millisecond) //Chilla
+		id1 := kad.NewRandomKademliaID()
+		contact1 := kad.NewContact(id1, myIP.String()+":8080")
+		node1 := kad.NewKademlia(contact1)
+		go node1.GetNetwork().Listen(contact1) //Starta min listen
+		node1.GetNetwork().SendFindContactMessage(&maincontact, contact1.ID) //Informera main om att jag finns
+		time.Sleep(10 * 1000 * time.Millisecond) //Chilla
+		node1.GetNetwork().GetRT().PrintRoutingTable() //Printa min RT
+	}
+
+
+
+}
+
+func mymain() {
 	fmt.Println("Listening... Give command")
 
 	//Skapar allt och startar listen på alla
@@ -37,5 +72,16 @@ func main() {
 	fmt.Println("Startar lookup")
 	svar := node1.LookupContact(&contact2)
 	fmt.Printf("Svaret blev","%v\n", svar)
+}
 
+func GetOutboundIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
 }
